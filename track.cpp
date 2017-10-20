5be3679628bd6135e73403d3ee5d3c9c2517c0ea
Naveen Tripathi <naveen.tripathi1010@gmail.com>
2017-10-20 12:02:59 +0530

    Added info for differnt algos, and observations

--- track.cpp
+++ track.cpp
@@ -1,3 +1,10 @@
+//Following are supported algos:
+//1. MIL
+//2. BOOSTING
+//3. MEDIANFLOW
+//4. TLD - Performing comparitively better than others, scale invariant, captures subject again once goes Out of Frame
+//5. KCF - Problematic when subject goes out of frame and comes back in, and also with scale.
+
 #include <opencv2/opencv.hpp>
 #include <opencv2/tracking.hpp>
 #include <iostream>
@@ -7,7 +14,7 @@ using namespace cv;
 
 int main(){
 	Mat frame;
-	Ptr<Tracker> tracker = Tracker::create("KCF");
+	Ptr<Tracker> tracker = Tracker::create("TLD");
 	VideoCapture cap(0);
 	int frameW, frameH;
 	frameW = cap.get(CV_CAP_PROP_FRAME_WIDTH);
@@ -19,7 +26,7 @@ int main(){
 	if(roi.width==0 || roi.height==0)
 		return -1;
 	tracker->init(frame,roi);
-	cout<<"Starting"<<endl;
+	cout<<"Starting Tracking"<<endl;
 	
 	while(check){
 		check = cap.read(frame);
@@ -31,5 +38,6 @@ int main(){
 		if(waitKey(100)==27)
 			break;
 	}
+	cout<<"Program Exit"<<endl;
 	return 0;	
 }
